049412542cc142fc0abe1293b26668caf750f525
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-19 14:12:00 +0200

    Esercizio portato a termine

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,42 @@ const wrapper = document.querySelector(".wrapper")
 generateBtn = wrapper.querySelector(".form button")
 console.log("Questo è il button presente dentro a form");
 console.log(generateBtn);
+/* Dichiaro la costante che va a pescare l'input */
+qrInput = wrapper.querySelector(".form input")
+/* QR Image */
+qrImg = wrapper.querySelector(".qr-code img")
+
 
 /* Ora al mio button devo lasciare un evento a cui reagire */
 generateBtn.addEventListener("click", () => {
-    console.log("Sono stato premuto");
+    /* console.log("Sono stato premuto"); */
+    let qrValue = qrInput.value
+    console.log(`Questa è la verifica del valore di qrValue ${qrValue}`);
+    /* Prevenire il click dell'utente in caso di input vuoto */
+    if (!qrValue) return; // Se l'input fosse vuoto/undefined non ritorna la classe Active
+
+    /* Al button, sul click il cambio testo durante caricamento */
+    generateBtn.innerText = "Sto generando il QR Code.."
+
+    /* Ora percorso del QR Code Generator con cambiamento alla query 
+    in base a ciò che viene scritto dall'utente */
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`
+    /* Ora lascio un evento alla mia immagine, al caricamento viene aggiunta
+    la classe active */
+    qrImg.addEventListener("load", () => {
+        /* In questo modo al mio wrapper lascio la classe active nel 
+        momento in cui il pulsante viene premuto*/
+        wrapper.classList.add("active")
+        /* Al button, sul click il cambio testo durante caricamento */
+        generateBtn.innerText = "Genera il QR Code"
+    })
+})
+
+/* Evento all'input in caso di click solo su tastiera (quindi enter senza nulla) */
+qrInput.addEventListener("keyup", () => {
+    /* Se l'input non andasse a buon fine */
+    if (!qrValue.value) {
+        /* Rimuovo la classe active */
+        wrapper.classList.remove("active")
+    }
 })
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Progetto Javascript QR Code Generator
+
+Una semplice web app di un QR Code generator sviluppato grazie a una API esterna. Linguaggi utilizzati per la creazione
+
+- HTML (Markup)
+- CSS (Markup)
+- JAVASCRIPT
+
+API presa da :
+
+- link : **https://goqr.me/api/**
+
+Condizione iniziale dell'app
+
+![Inizio](./js-qr-condizione_iniziale.png)
+
+Condizione finale dell'app, dopo che ha generato il QR Code
+
+![Inizio](./js-qr-condizione_finale.png)
