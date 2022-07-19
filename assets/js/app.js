4197a0abc24b7064bb1a07199293f14e242b2d0b
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-19 13:20:23 +0200

    Aggiunto evento al button di creazione qr code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-console.log("Ciao");
+/* console.log("Ciao"); */
+
+/* Dichiaro una variabile wrapper dove vado a pescare il mio div */
+const wrapper = document.querySelector(".wrapper")
+/* Dichiaro il button per la generazione del QR */
+/* Essendo il button parente di wrapper lo pesco da li */
+generateBtn = wrapper.querySelector(".form button")
+console.log("Questo è il button presente dentro a form");
+console.log(generateBtn);
+
+/* Ora al mio button devo lasciare un evento a cui reagire */
+generateBtn.addEventListener("click", () => {
+    console.log("Sono stato premuto");
+})
